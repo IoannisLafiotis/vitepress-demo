a421bbad2f33b1b752e9ef5dda035c27a4266ad6
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-11-27 17:36:15 +0100

    deploy

---
+++ docs/.vitepress/dist/assets/Shopizer-TLS.b0ecac2a.js
@@ -0,0 +1,1 @@
+var s="/vitepress-demo/assets/Shopizer-TLS.d6599970.png";export{s as _};
---
+++ docs/.vitepress/dist/assets/process_NewPagesHowto.md.ae2a4949.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a}from"./app.77865e86.js";const l='{"title":"","description":"","frontmatter":{},"relativePath":"metrics/analytics.md","lastUpdated":1638028870235}',o={};function r(s,c,n,p,i,_){return a(),e("div")}var f=t(o,[["render",r]]);export{l as __pageData,f as default};
